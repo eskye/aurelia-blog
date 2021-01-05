d5a7ba86b35d17d22de4cf13c8a751103b4e8e12
eskye <sunkanmiijatuyi@gmail.com>
2021-01-05 17:02:38 +0100

    ft/install-plugins

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,18 @@
+import { PLATFORM } from 'aurelia-pal';
+import {Router, RouterConfiguration} from 'aurelia-router'; 
 export class App {
   public message = 'Hello World!';
+  router: Router;
+
+  configureRouter(config: RouterConfiguration, router: Router){
+    config.title = 'Contacts';
+    config.options.pushState = true;
+    config.options.root = '/';
+    config.map([
+      {route: '', moduleId: PLATFORM.moduleName('no-selection'), title: 'Select'},
+      {route: 'contacts/:id', moduleId: PLATFORM.moduleName('contact-detail'), title: 'contacts'},
+    ]);
+
+    this.router = router;
+  }
 }
